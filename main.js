13e2a7bdff700551e119d8fc673557fa47034ec4
Shawn Carter <53487411+ShawnCart3r@users.noreply.github.com>
2024-11-23 21:24:27 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -33,3 +33,9 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+document.querySelectorAll('.card').forEach((card) => {
+  card.addEventListener('click', () => {
+      card.classList.toggle('is-flipped');
+  });
+});
